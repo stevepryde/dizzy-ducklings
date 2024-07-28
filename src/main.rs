212e3e107b5d3c8e85606ad77875d7e725dbe12d
Steve Pryde <steve@stevepryde.com>
2024-07-28 12:01:56 +0930

    Change the name

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
-use wheel_escape::AppPlugin;
+use dizzy_ducklings::AppPlugin;
 
 fn main() -> AppExit {
     App::new().add_plugins(AppPlugin).run()
